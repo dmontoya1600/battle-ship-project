8e59bb44b5240df256b967b95d0d69cfb19d5c0b
Taylor H <taylor@htmlartist.net>
2021-03-10 16:06:29 -0500

    phase 1 complete

--- starter/humanPlayer.js
+++ starter/humanPlayer.js
@@ -1,19 +1,32 @@
+const readline = require("readline");
 class HumanPlayer {
   constructor() {
+    this.rl = readline.createInterface(process.stdin, process.stdout);
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
   }
 
+
+  getMove(processMove) {
+    this.rl.question("Choose a coordinate to hit. (e.g. 3,1) \n> ", (answer) => {
+      const [row, col] = answer.split(",");
+      processMove([row, col]);
+    });
+  }
   getMove() {
     // TODO: Ask the user for their move and process the answer using the rl
     // interface.Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
   }
 
-  processGameOver(isWon) {
-    // TODO: Display a different message depending on if the player won or lost
-    // the game. Close the rl interface.
+  processGameOver(isWon, turns) {
+    if (isWon) {
+      console.log(`Nice job, you won in ${turns} turns!`);
+    } else {
+      console.log("You lose!");
+    }
+    this.rl.close();
   }
 }
 
